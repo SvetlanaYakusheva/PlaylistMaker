d9e63995bec70442c6c9c3dbd3a894957378cd43
Svetlana Yakusheva <onetwoquestion@ya.ru>
2025-10-01 19:36:04 +0300

    17 спринт. Версия 1: -применен DI с использованием Koin

---
+++ app/src/main/java/com/practicum/playlistmaker/di/InteractorModule.kt
@@ -0,0 +1,30 @@
+package com.practicum.playlistmaker.di
+
+import com.practicum.playlistmaker.search.domain.api.SearchHistoryInteractor
+import com.practicum.playlistmaker.search.domain.api.TrackListInteractor
+import com.practicum.playlistmaker.search.domain.impl.SearchHistoryInteractorImpl
+import com.practicum.playlistmaker.search.domain.impl.TrackListInteractorImpl
+import com.practicum.playlistmaker.settings.domain.NightModeSettingsInteractor
+import com.practicum.playlistmaker.settings.domain.impl.NightModeSettingsInteractorImpl
+import com.practicum.playlistmaker.sharing.domain.SharingInteractor
+import com.practicum.playlistmaker.sharing.domain.impl.SharingInteractorImpl
+import org.koin.dsl.module
+
+val interactorModule = module {
+
+    single<TrackListInteractor> {
+        TrackListInteractorImpl(get())
+    }
+
+    single<SearchHistoryInteractor> {
+        SearchHistoryInteractorImpl(get())
+    }
+
+    single<NightModeSettingsInteractor> {
+        NightModeSettingsInteractorImpl(get())
+    }
+
+    single<SharingInteractor> {
+        SharingInteractorImpl(get(), get())
+    }
+}
